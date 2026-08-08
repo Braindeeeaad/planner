a938b91e16ef22f9897d485516220276bdc51c94
Braindeeeaad <indrajith2048@gmail.com>
2026-08-07 20:39:48 -0500

    basic database creation code working

---
+++ src-tauri/src/db/connection.rs
@@ -0,0 +1,21 @@
+use sqlx::{migrate::MigrateDatabase, Sqlite}; 
+
+const DB_URL: &str = "sqlite://sqlite.db"; 
+ 
+pub async fn create_database(){
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false){
+        println!("Creating database {}",DB_URL);
+        match Sqlite::create_database(DB_URL).await{
+            Ok(_) => println!("Create db success"), 
+            Err(error) => panic!("error: {}",error),
+        }
+    } else {
+        println!("Database already exists");
+    }
+}
+
+#[tokio::test(name="db_connection_test")]
+async fn db_connection_test(){
+    create_database().await;
+    assert!(true);
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let return_str = format!("Hello, {}! You've been greeted from Rust!", name);
+    return_str
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -11,4 +12,21 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+pub fn add(left: u64, right: u64)->u64{
+    right+left 
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_eq;
+
+    use super::*; 
+    
+    #[test] 
+    fn exploration() {
+        let result = add(2,2); 
+        assert_eq!(result,4);
+    }
 }
